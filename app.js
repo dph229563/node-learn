ee66e6f6997ab80c75c03cde2874becb0b137fae
xiaoming <dphhxy@163.com>
2025-07-04 10:34:48 +0800

    fix: node learn

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var adminArticleRouter = require('./routes/admin/article');
 
 var app = express();
 
@@ -16,5 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/admin/articles', adminArticleRouter);
 
 module.exports = app;
