8701316163540608234b7eff02a8a9c8d4c5a328
xiaoming <dphhxy@163.com>
2025-12-03 11:46:12 +0800

    fix: 修改部署前的错误

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+
+require("dotenv").config();
+
 const adminAuth = require("./middlewares/admin-auth");
 const userAuth = require('./middlewares/user-auth');
-require("dotenv").config();
+
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
